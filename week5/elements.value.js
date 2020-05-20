85fef75abecc43748a6e662407e77796db2ec256
BrenSeibel <brens.seibel@gmail.com>
2020-05-20 17:06:09 -0400

    fixed

--- week5/elements.value.js
+++ week5/elements.value.js
@@ -1,1 +1,18 @@
-alert("banana")
+var rainbow = document.getElementsByTagName("form")[0]
+console.log(rainbow)
+rainbow.addEventListener("submit", (event) => {
+event.preventDefault()
+    console.log(rainbow.children)
+    console.log(rainbow.children[1].value)
+    console.log(rainbow.children[3].value)
+    console.log(rainbow.children[5].value)
+    
+    var values = {
+        firstName:rainbow.children[1].value,
+        lastName:rainbow.children[3].value,
+        age:rainbow.children[5].value,
+    }
+
+    alert(JSON.stringify(values))
+})
+
